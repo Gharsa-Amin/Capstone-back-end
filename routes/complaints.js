8e428c6f2ead3e6b39e078f57a14cb5c3bfe7bb4
Gharsa Amin <ghars0718@gmail.com>
2025-01-13 17:34:02 -0500

    created a middleware for authentication

--- routes/complaints.js
+++ routes/complaints.js
@@ -5,36 +5,6 @@ const db = initKnex(knexfile); // Use knexfile to initialize knex
 import express from "express";
 const router = express.Router();
 
-// router.post("/", async (req, res) => {
-// 	const { name, email, complaints } = req.body;
-
-// 	console.log(req.body); // Log incoming request body for debugging
-
-// 	// Validate incoming data
-// 	if (!name || !email || !complaints) {
-// 		return res
-// 			.status(400)
-// 			.send("Missing required fields (name, email, complaint)");
-// 	}
-
-// 	try {
-// 		const [id] = await db("complaints").insert({
-// 			name,
-// 			email,
-// 			complaints, // Store complaints in the "complaints" column
-// 		});
-
-// 		return res.status(201).json({
-// 			message: "Complaint submitted successfully!",
-// 			complaint_id: id,
-// 		});
-// 	} catch (error) {
-// 		console.error(error);
-// 		res
-// 			.status(500)
-// 			.json({ message: "Internal Server Error", error: error.message });
-// 	}
-// });
 router.post("/", async (req, res) => {
 	const { name, email, complaints } = req.body;
 
--- routes/users.js
+++ routes/users.js
@@ -47,16 +47,21 @@ router.post("/register", async (req, res) => {
 
 router.post("/onboarding", async (req, res) => {
 	console.log(req.body);
-	// if (
-	// 	!req.body.name ||
-	// 	!req.body.phonenumber ||
-	// 	!req.body.email ||
-	// 	!req.body.password
-	// ) {
-	// 	return res.status(400).json({
-	// 		msg: "You must provide a name, phonenumber, email and password",
-	// 	});
-	// }
+	if (
+		!req.body.networth ||
+		!req.body.risk_tolerance ||
+		!req.body.trading_experience ||
+		!req.body.products ||
+		!req.body.top_crypto_coins ||
+		!req.body.current_occupation ||
+		!req.body.scammed_before ||
+		!req.body.someone_helping ||
+		!req.body.shared_access
+	) {
+		return res.status(400).json({
+			msg: "You must fill all the required fields",
+		});
+	}
 	try {
 		// Create a new user profile in the database
 		const newUserProfileIds = await db("user_profile").insert({
@@ -83,55 +88,46 @@ router.post("/onboarding", async (req, res) => {
 	}
 });
 
-// router.post("/login", async (req, res) => {
-// 	if (!req.body.email || !req.body.password) {
-// 		return res
-// 			.status(400)
-// 			.json({ msg: "You must provide an email and password" });
-// 	}
+router.post("/login", async (req, res) => {
+	if (!req.body.email || !req.body.password) {
+		return res
+			.status(400)
+			.json({ msg: "You must provide an email and a password!" });
+	}
 
-// 	try {
-// 		// Query the DB for a user with the email address provided in the request body
-// 		const user = await knex("users").where({ email: req.body.email }).first();
+	try {
+		const user = await knex("user").where({ email: req.body.email }).first();
+		const result = await bcrypt.compare(req.body.password, user.password);
+		if (!result) {
+			return res
+				.status(403)
+				.json({ message: "Username/Password combination is not correct" });
+		}
 
-// 		// Use bcrypt to check whether the password sent in the request body matches the hashed password in the DB
-// 		const result = await bcrypt.compare(req.body.password, user.password);
+		const token = jwt.sign(
+			{
+				id: user.id,
+				sub: user.email,
+			},
+			process.env.JWT_SECRE,
+			{ expiresIn: "8h" }
+		);
+		res.json({ authToken: token });
+	} catch (error) {
+		res.status(400).json({ message: "User not found" });
+	}
+});
 
-// 		if (!result) {
-// 			return res
-// 				.status(403)
-// 				.json({ message: "Username/Password combination is incorrect" });
-// 		}
+router.get("/profile", authorise, async (req, res) => {
+	try {
+		// The `authorise` middleware added the decoded token to `req.token` so we have the users ID from the JWT token.
+		// Query the DB for a user with that ID.
+		const user = await knex("users").where({ id: req.token.id }).first();
 
-// 		// Use the jwt library to generate a JWT token for the user.
-// 		// Include the id and email of the user in the payload of the JWT
-// 		// We can access the data in the payload of the JWT later (when the user accesses a protected page)
-// 		const token = jwt.sign(
-// 			{
-// 				id: user.id,
-// 				sub: user.email,
-// 			},
-// 			process.env.JWT_SECRET,
-// 			{ expiresIn: "8h" }
-// 		);
+		res.json(user);
+	} catch (error) {
+		res.status(500).json({ message: "Can't fetch user profile" });
+	}
+});
 
-// 		// Send the JWT token to the client
-// 		res.json({ authToken: token });
-// 	} catch (error) {
-// 		res.status(400).json({ message: "User not found" });
-// 	}
-// });
-
-// // The `authorise` middleware here checks if the user is logged in before processing this endpoint
-// router.get("/profile", authorise, async (req, res) => {
-// 	try {
-// 		// The `authorise` middleware added the decoded token to `req.token` so we have the users ID from the JWT token.
-// 		// Query the DB for a user with that ID.
-// 		const user = await knex("users").where({ id: req.token.id }).first();
-
-// 		res.json(user);
-// 	} catch (error) {
-// 		res.status(500).json({ message: "Can't fetch user profile" });
-// 	}
-// });
 export default router;
